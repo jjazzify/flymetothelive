ccd1eeede42edba8589ec0a9a6a4a42103e078a5
HOJEONGKIMM <by08035@naver.com>
2024-08-18 21:30:03 +0900

    Add : initail backend structure

---
+++ backend/flymetothlive/src/main/java/com/jazzify/flymetothlive/FlymetothliveApplication.java
@@ -0,0 +1,13 @@
+package com.jazzify.flymetothlive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlymetothliveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlymetothliveApplication.class, args);
+	}
+
+}
---
+++ backend/flymetothlive/src/test/java/com/jazzify/flymetothlive/FlymetothliveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jazzify.flymetothlive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlymetothliveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
