a64f6f9d709f23de54d7a71bdd2b083b0b7ff86b
HOJEONGKIMM <by08035@naver.com>
2024-10-27 11:12:07 +0900

    BE: [add] 백엔드 프로젝트 생성 #1

---
+++ backend/Eyesee/src/main/java/com/fortune/eyesee/EyeseeApplication.java
@@ -0,0 +1,13 @@
+package com.fortune.eyesee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyeseeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EyeseeApplication.class, args);
+    }
+
+}
---
+++ backend/Eyesee/src/test/java/com/fortune/eyesee/EyeseeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fortune.eyesee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EyeseeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
